49aba8a78a40e0de75b936458d882ed6417dbcee
MinHyeJun <m2j9702@naver.com>
2017-01-06 21:31:49 +0900

    edittext 특정 길이 이상 입력 감지 실습

--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -3,10 +3,11 @@ package m2j9702.test.practice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,18 +17,50 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ProgressBar progress = (ProgressBar) findViewById(R.id.my_progress);
-        Button button = (Button) findViewById(R.id.my_button);
+        final EditText editInput = (EditText) findViewById(R.id.edit_input);
+        final Button btn = (Button) findViewById(R.id.btn);
 
-        button.setOnClickListener(new View.OnClickListener()
+        editInput.addTextChangedListener(new TextWatcher()
         {
             @Override
-            public void onClick(View v)
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
             {
-                progress.setProgress(progress.getProgress() + 5);
 
-                Log.i("jkllkjlkl", progress.getProgress()+"");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                Log.i("asdf", "On key");
+                if(s.toString().length() >= 8)
+                    btn.setEnabled(true);
+                else
+                    btn.setEnabled(false);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
             }
         });
+        /*
+        editInput.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                Log.i("asdf", "On key");
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    if(editInput.getText().toString().length() >= 8)
+                        btn.setEnabled(true);
+                    else
+                        btn.setEnabled(false);
+                }
+                return true;
+            }
+        });*/
     }
 }
