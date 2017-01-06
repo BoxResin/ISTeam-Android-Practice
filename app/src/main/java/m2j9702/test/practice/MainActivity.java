ae815e6361d6d4cde87d4b4f175d3611c7ce0034
MinHyeJun <m2j9702@naver.com>
2017-01-06 20:47:20 +0900

    checkbox 실습

--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -3,6 +3,9 @@ package m2j9702.test.practice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.my_checkbox);
+        Button button = (Button) findViewById(R.id.my_button);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                checkBox.setChecked(!checkBox.isChecked());
+            }
+        });
     }
 }
