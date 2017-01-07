ddca9da5d8bc93b64f23ab9eed798c412aea6b22
MinHyeJun <m2j9702@naver.com>
2017-01-07 22:30:46 +0900

    액티비티 생명주기 메소드 과제

--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -3,64 +3,61 @@ package m2j9702.test.practice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+
+        Log.d("ASDF", "OnRestart");
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText editInput = (EditText) findViewById(R.id.edit_input);
-        final Button btn = (Button) findViewById(R.id.btn);
+        Log.d("ASDF", "OnCreate");
+    }
 
-        editInput.addTextChangedListener(new TextWatcher()
-        {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after)
-            {
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("ASDF", "OnStart");
+    }
 
-            }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
-            {
-                Log.i("asdf", "On key");
-                if(s.toString().length() >= 8)
-                    btn.setEnabled(true);
-                else
-                    btn.setEnabled(false);
+        Log.d("ASDF", "OnResume");
+    }
 
-            }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("ASDF", "OnPause");
+    }
 
-            @Override
-            public void afterTextChanged(Editable s)
-            {
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("ASDF", "OnStop");
+    }
 
-            }
-        });
-        /*
-        editInput.setOnKeyListener(new View.OnKeyListener()
-        {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event)
-            {
-                Log.i("asdf", "On key");
-                if (event.getAction() == KeyEvent.ACTION_DOWN)
-                {
-                    if(editInput.getText().toString().length() >= 8)
-                        btn.setEnabled(true);
-                    else
-                        btn.setEnabled(false);
-                }
-                return true;
-            }
-        });*/
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("ASDF", "OnDestroy");
     }
 }
