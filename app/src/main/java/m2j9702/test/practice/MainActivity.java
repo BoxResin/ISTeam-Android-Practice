1221367c3e54bf465f6abbf3ee97d10e9800e8d5
MinHyeJun <m2j9702@naver.com>
2017-01-08 10:30:43 +0900

    로그인 창 만들기

--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -3,18 +3,22 @@ package m2j9702.test.practice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
 
-    @Override
-    protected void onRestart()
-    {
-        super.onRestart();
-
-        Log.d("ASDF", "OnRestart");
-    }
+    private Button btnLogin;
+    private EditText editId;
+    private EditText editPassword;
+    private TextView textNotice;
+    private boolean editIdState = false;
+    private boolean editPasswordState = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -22,42 +26,85 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("ASDF", "OnCreate");
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        editId = (EditText) findViewById(R.id.edit_id);
+        editPassword = (EditText) findViewById(R.id.edit_password);
+        textNotice = (TextView) findViewById(R.id.text_notice);
+
+        editId.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if(s.toString().length() > 0)
+                {
+                    editIdState = true;
+                    checkStates();
+
+                }
+                else
+                {
+                    editIdState = false;
+                    checkStates();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
+            }
+        });
+        editPassword.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if(s.toString().length() > 0)
+                {
+                    editPasswordState = true;
+                    checkStates();
+
+                }
+                else
+                {
+                    editPasswordState = false;
+                    checkStates();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
+            }
+        });
     }
 
-    @Override
-    protected void onStart()
+    void checkStates()
     {
-        super.onStart();
-        Log.d("ASDF", "OnStart");
+        if(editIdState && editPasswordState)
+        {
+            btnLogin.setEnabled(true);
+            textNotice.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            btnLogin.setEnabled(false);
+            textNotice.setVisibility(View.VISIBLE);
+        }
     }
 
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-
-        Log.d("ASDF", "OnResume");
-    }
-
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-        Log.d("ASDF", "OnPause");
-    }
-
-    @Override
-    protected void onStop()
-    {
-        super.onStop();
-        Log.d("ASDF", "OnStop");
-    }
-
-    @Override
-    protected void onDestroy()
-    {
-        super.onDestroy();
-        Log.d("ASDF", "OnDestroy");
-    }
 }
