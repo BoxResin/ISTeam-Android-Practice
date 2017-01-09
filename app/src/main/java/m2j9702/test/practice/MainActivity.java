8910fc79ec518fbb96a3c7e6d5cb0bf37bcf98ad
BoxResin <boxresin@gmail.com>
2017-01-09 19:34:32 +0900

    뷰 가져오기, 버튼 이벤트 처리하기 실습

--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -1,31 +1,43 @@
 package m2j9702.test.practice;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private TextView txt1;
+    private TextView txt2;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnMove = (Button) findViewById(R.id.btn_move);
-        btnMove.setOnClickListener(new View.OnClickListener()
+        txt1 = (TextView) findViewById(R.id.txt);
+        txt2 = (TextView) findViewById(R.id.txt2);
+        Button btnTest = (Button) findViewById(R.id.btn_test);
+        Button btnTest2 = (Button) findViewById(R.id.btn_test2);
+
+        btnTest.setOnClickListener(this);
+        btnTest2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.btn_test)
         {
-            @Override
-            public void onClick(View view)
-            {
-                // 자바 리플렉션
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+            txt1.setTextColor(0xFFFF0000);
+        }
+        else if (view.getId() == R.id.btn_test2)
+        {
+            txt2.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 100);
+        }
     }
 }
