e1700505e8636136385e2aa5ecf0a8929baa29ec
cys77135 <cys77135@ssu.ac.kr>
2017-01-16 01:10:59 +0900

    로그인 화면 만들기 최종본

--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -3,29 +3,94 @@ package m2j9702.test.practice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    String idstr, pwdstr;
+    private TextView warn;
+    private Button btn;
+    int l1 = 0, l2 = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView warn = (TextView) findViewById(R.id.warn);
+
         EditText id = (EditText) findViewById(R.id.id);
         EditText pwd = (EditText) findViewById(R.id.pwd);
-        Button btn = (Button) findViewById(R.id.btn);
+        warn = (TextView) findViewById(R.id.warn);
+        btn = (Button) findViewById(R.id.btn);
 
-        idstr = id.getText().toString();
-        pwdstr = pwd.getText().toString();
-        if ((idstr.getBytes().length != 0) && (pwdstr.getBytes().length != 0))
-        {
-            warn.setVisibility(warn.GONE);
-        }
+        id.addTextChangedListener(iid);
+        pwd.addTextChangedListener(ppwd);
+        btn.setEnabled(false);
     }
+
+    TextWatcher iid = new TextWatcher()
+    {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+        {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+            l1 = s.length();
+            if ((l1 != 0) && (l2 != 0))
+            {
+                warn.setVisibility(View.INVISIBLE);
+                btn.setTextColor(0xFF000000);
+                btn.setEnabled(true);
+            }
+            else
+            {
+                warn.setVisibility(View.VISIBLE);
+                btn.setTextColor(0xFF9c9898);
+                btn.setEnabled(false);
+            }
+        }
+    };
+
+    TextWatcher ppwd = new TextWatcher()
+    {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+        {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+            l2 = s.length();
+            if ((l1 != 0) && (l2 != 0))
+            {
+                warn.setVisibility(View.INVISIBLE);
+                btn.setTextColor(0xFF000000);
+                btn.setEnabled(true);
+            }
+            else
+            {
+                warn.setVisibility(View.VISIBLE);
+                btn.setTextColor(0xFF9c9898);
+                btn.setEnabled(false);
+            }
+        }
+    };
 }
