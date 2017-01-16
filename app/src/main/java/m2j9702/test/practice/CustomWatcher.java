e8f8c0315bfe2832c77905ba4910e0ed2fa3539c
cys77135 <cys77135@ssu.ac.kr>
2017-01-16 13:57:43 +0900

    회원가입 창 만들기-중간 단계

--- app/src/main/java/m2j9702/test/practice/CustomWatcher.java
+++ app/src/main/java/m2j9702/test/practice/CustomWatcher.java
@@ -4,22 +4,34 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import static m2j9702.test.practice.R.id.id;
-import static m2j9702.test.practice.R.id.pwd;
+import static m2j9702.test.practice.R.id.email;
+import static m2j9702.test.practice.R.id.nick;
+import static m2j9702.test.practice.R.id.pwd1;
+import static m2j9702.test.practice.R.id.pwd2;
 
 public class CustomWatcher implements TextWatcher
 {
-    private View view;
-    private TextView warn;
-    private Button btn;
+    private View view1, view2;
+    private TextView warn1,warn2,warn3,warn4;
+    private Button regist;
+    boolean checkemail = false, checkpwd = false;
+    private String emailstr = new String();
+    private String str1 = new String();
+    private String str2 = new String();
 
-    public CustomWatcher(View view, TextView warn, Button btn)
+
+    public CustomWatcher(View view1, View view2, TextView warn1, TextView warn2,TextView warn3,TextView warn4, Button regist)
     {
-        this.view = view;
-        this.warn = warn;
-        this.btn = btn;
+        this.view1 = view1;
+        this.view2 = view2;
+        this.warn1 = warn1;
+        this.warn2 = warn2;
+        this.warn3 = warn3;
+        this.warn4 = warn4;
+        this.regist = regist;
     }
 
     @Override
@@ -37,26 +49,110 @@ public class CustomWatcher implements TextWatcher
     @Override
     public void afterTextChanged(Editable s)
     {
-        switch (view.getId())
+        switch (view1.getId())
         {
-            case id:
-                MainActivity.l1 = s.length();
+            case email:
+                checkemail = checkEmail((EditText) view1);
                 break;
-            case pwd:
+            case nick:
                 MainActivity.l2 = s.length();
                 break;
+            case pwd1:
+                MainActivity.l3 = s.length();
+                break;
+            case pwd2:
+                checkpwd = checkPwd((EditText) view1, (EditText) view2);
+                break;
         }
-        if ((MainActivity.l1 != 0) && (MainActivity.l2 != 0))
+        if (checkemail)
         {
-            warn.setVisibility(View.INVISIBLE);
-            btn.setTextColor(0xFF000000);
-            btn.setEnabled(true);
+            warn1.setVisibility(View.INVISIBLE);
         }
         else
         {
-            warn.setVisibility(View.VISIBLE);
-            btn.setTextColor(0xFF9c9898);
-            btn.setEnabled(false);
+            warn1.setVisibility(View.VISIBLE);
+            regist.setVisibility(View.GONE);
+        }
+        if (MainActivity.l2 != 0)
+        {
+            warn2.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            warn2.setVisibility(View.VISIBLE);
+            regist.setVisibility(View.GONE);
+        }
+        if (MainActivity.l3 >= 8)
+        {
+            warn3.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            warn3.setVisibility(View.VISIBLE);
+            regist.setVisibility(View.GONE);
+        }
+        if (checkpwd)
+        {
+            warn4.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            warn4.setVisibility(View.VISIBLE);
+            regist.setVisibility(View.GONE);
+        }
+        if((checkemail)&&(MainActivity.l2 != 0)&&(MainActivity.l3 >= 8)&&(checkpwd))
+        {
+            regist.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            regist.setVisibility(View.GONE);
+        }
+    }
+
+    public boolean checkEmail(EditText email)
+    {
+        int ck1 = 0, ck2 = 0;
+        int j = email.length() - 1;
+        emailstr = email.getText().toString();
+        MainActivity.l1 = email.length();
+        for (int i = 0; i < email.length(); i++)
+        {
+            if (emailstr.charAt(i) == '@')
+            {
+                j = i;
+                ck1++;
+            }
+            else if (emailstr.charAt(i) == '.')
+            {
+                if (j < i)
+                {
+                    ck2++;
+                }
+            }
+        }
+        if ((MainActivity.l1 >= 5) && (ck1 == 1) && (ck2 == 1))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean checkPwd(EditText pwd1, EditText pwd2)
+    {
+        str1=pwd1.getText().toString();
+        str2=pwd2.getText().toString();
+        MainActivity.l4 = pwd1.length();
+        if ((MainActivity.l4 >= 8) && (pwd1.length() == pwd2.length()) && (str1.equals(str2)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -3,17 +3,20 @@ package m2j9702.test.practice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import static android.view.View.GONE;
+
 public class MainActivity extends AppCompatActivity
 {
-    private TextView warn;
-    private EditText id;
-    private EditText pwd;
-    private Button btn;
-    public static int l1 = 0, l2 = 0;
+    private TextView warn1, warn2, warn3, warn4;
+    private EditText email, nick, pwd1, pwd2;
+    private Button regist;
+    private View space;
+    public static int l1 = 0, l2 = 0, l3 = 0, l4 = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -21,14 +24,20 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        warn = (TextView) findViewById(R.id.warn);
-        id = (EditText) findViewById(R.id.id);
-        pwd = (EditText) findViewById(R.id.pwd);
-        btn = (Button) findViewById(R.id.btn);
+        warn1 = (TextView) findViewById(R.id.warn1);
+        warn2 = (TextView) findViewById(R.id.warn2);
+        warn3 = (TextView) findViewById(R.id.warn3);
+        warn4 = (TextView) findViewById(R.id.warn4);
+        email = (EditText) findViewById(R.id.email);
+        nick = (EditText) findViewById(R.id.nick);
+        pwd1 = (EditText) findViewById(R.id.pwd1);
+        pwd2 = (EditText) findViewById(R.id.pwd2);
+        regist = (Button) findViewById(R.id.regist);
 
-        btn.setEnabled(false);
-        id.addTextChangedListener(new CustomWatcher(id, warn, btn));
-        pwd.addTextChangedListener(new CustomWatcher(pwd, warn, btn));
-        btn.setEnabled(false);
+        regist.setVisibility(GONE);
+        email.addTextChangedListener(new CustomWatcher(email, space, warn1, warn2, warn3, warn4, regist));
+        nick.addTextChangedListener(new CustomWatcher(nick, space, warn1, warn2, warn3, warn4, regist));
+        pwd1.addTextChangedListener(new CustomWatcher(pwd1, space, warn1, warn2, warn3, warn4, regist));
+        pwd2.addTextChangedListener(new CustomWatcher(pwd2, pwd1, warn1, warn2, warn3, warn4, regist));
     }
 }
