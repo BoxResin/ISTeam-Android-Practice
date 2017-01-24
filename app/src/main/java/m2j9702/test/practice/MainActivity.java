2d5a4aa9c3b3da7dac2875b42df54859ccc75d78
BoxResin <boxresin@gmail.com>
2017-01-24 11:34:24 +0900

    액티비티 여러 개 만들기, 액티비티간 데이터 주고받기 실습

--- app/src/main/java/m2j9702/test/practice/MainActivity.java
+++ app/src/main/java/m2j9702/test/practice/MainActivity.java
@@ -1,43 +1,55 @@
 package m2j9702.test.practice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    private TextView txt1;
-    private TextView txt2;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt1 = (TextView) findViewById(R.id.txt);
-        txt2 = (TextView) findViewById(R.id.txt2);
-        Button btnTest = (Button) findViewById(R.id.btn_test);
-        Button btnTest2 = (Button) findViewById(R.id.btn_test2);
-
-        btnTest.setOnClickListener(this);
-        btnTest2.setOnClickListener(this);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view)
+    public void onClick(View v)
     {
-        if (view.getId() == R.id.btn_test)
+        Intent intent = new Intent(this, SecondActivity.class);
+        EditText edit = (EditText) findViewById(R.id.edit);
+
+        intent.putExtra("문자열", edit.getText().toString());
+
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == 1)
         {
-            txt1.setTextColor(0xFFFF0000);
+            if (resultCode == RESULT_OK)
+            {
+                if (data != null)
+                {
+                    int intData = data.getIntExtra("asdf", 0);
+                    Toast.makeText(this, "" + intData, Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if (resultCode == RESULT_CANCELED)
+            {
+            }
         }
-        else if (view.getId() == R.id.btn_test2)
-        {
-            txt2.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 100);
-        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/m2j9702/test/practice/SecondActivity.java
@@ -0,0 +1,56 @@
+package m2j9702.test.practice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * 두 번째 액티비티
+ */
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener
+{
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+
+		Intent intent = getIntent();
+		String data = intent.getStringExtra("문자열");
+
+		Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+
+		Button btnOk = (Button) findViewById(R.id.btn_ok);
+		Button btnCancel = (Button) findViewById(R.id.btn_cancel);
+
+		btnOk.setOnClickListener(this);
+		btnCancel.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		// 눌린 버튼이 확인 버튼일 때
+		if (v.getId() == R.id.btn_ok)
+		{
+			EditText editNumber = (EditText) findViewById(R.id.edit_number);
+
+			Intent i = new Intent();
+			i.putExtra("asdf", Integer.parseInt(editNumber.getText().toString()));
+			setResult(RESULT_OK, i);
+		}
+
+		// 눌린 버튼이 취소 버튼일 때
+		else if (v.getId() == R.id.btn_cancel)
+		{
+			setResult(RESULT_CANCELED);
+		}
+
+		finish();
+	}
+}
